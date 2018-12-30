5afd1e7af13ed8ef00b9d382792a5f1d29a1c812
hyeongyu.yang <hyeongyu.yang@meshkorea.net>
2018-12-30 11:39:33 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Yang Hyeon Gyu on 30/12/2018.
+//  Copyright © 2018 Yang Hyeon Gyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
